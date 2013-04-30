8de58ec6d5bd39630e061e0d60d3961821b1aa0d
Éverton Ribeiro & Renato Albano <nuxlli@gmail.com>
2013-04-30 20:57:43 -0300

    Adding lager to beautiful logs ;)

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Plowman.Mixfile do
   # Configuration for the OTP application
   def application do
     [
-      applications: [:crypto, :public_key, :ssl, :ssh, :hackney],
+      applications: [:crypto, :public_key, :ssl, :ssh, :hackney, :lager],
       env: [
         binding: '0.0.0.0',
         port: 3333,
@@ -31,7 +31,9 @@ defmodule Plowman.Mixfile do
       {:hackney  , "0.4.0", [github: "benoitc/hackney", tag: "0.4.0"]},
       {:meck     , "0.7.2", [github: "eproxus/meck", branch: "develop"]},
       {:uuid     , "0.4.3", [github: "avtobiff/erlang-uuid", branch: "master"]},
-      {:exjson   , "0.0.1", [github: "azukiapp/exjson", branch: "master"]}
+      {:exjson   , "0.0.1", [github: "azukiapp/exjson", branch: "master"]},
+      {:goldrush , "0.1.0", [github: "DeadZen/goldrush", tag: "7ff9b03"]},
+      {:lager    , "2.0.0", [github: "basho/lager", tag: "2.0.0rc2"]}
     ]
   end
 end
