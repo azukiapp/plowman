1c865607c88e8eb462d0da27c848d75c447ac242
Éverton Ribeiro & Renato Albano <nuxlli@gmail.com>
2013-05-01 02:45:28 -0300

    Now Connection support forward messages to ssh client.

--- lib/plowman/connection.ex
+++ lib/plowman/connection.ex
@@ -1,17 +1,39 @@
 defmodule Plowman.Connection do
+  alias Plowman.GitCli.CliState, as: CliState
+
   def reply_failure(cm, wantReply, channel, msg // nil) do
-    if msg != nil, do: write_chars(cm, channel, msg)
+    if msg != nil, do: write_chars(cm, channel, msg, 1)
     :ssh_connection.reply_request(cm, wantReply, :failure, channel)
   end
 
-  defp write_chars(cm, channel, chars) when is_bitstring(chars) do
-    write_chars(cm, channel, bitstring_to_list(chars))
+  def write_chars(cm, channel, chars) do
+    write_chars(cm, channel, chars, 0)
   end
 
-  defp write_chars(cm, channel, chars) do
+  def write_chars(cm, channel, << chars :: binary >>, type) do
+    write_chars(cm, channel, bitstring_to_list(chars), type)
+  end
+
+  def write_chars(cm, channel, chars, type) do
     case :erlang.iolist_size(chars) do
-      0 -> {:ok}
-      _ -> :ssh_connection.send(cm, channel, 1, chars)
+      0 -> :ok
+      _ -> :ssh_connection.send(cm, channel, type, chars)
+    end
+  end
+
+  def forward(CliState[cm: cm, channel: channelId], msgs) do
+    case msgs do
+      <<?E, ?:, data :: binary >> ->
+        write_chars(cm, channelId, data, 1)
+      << data :: binary >> ->
+        write_chars(cm, channelId, data, 0)
+      :eof ->
+        :ssh_connection.exit_status(cm, channelId, 0)
+        :ssh_connection.close(cm, channelId)
+      {:error, _reason} ->
+        reply_failure(cm, false, channelId, "! Unable to contact build server.")
+        :ssh_connection.exit_status(cm, channelId, 1)
+        :ssh_connection.close(cm, channelId)
     end
   end
 end
--- test/plowman/connection_test.exs
+++ test/plowman/connection_test.exs
@@ -2,32 +2,68 @@ Code.require_file "../../test_helper.exs", __FILE__
 
 defmodule PlowmanConnectionTest do
   use Plowman.Test, async: false
-  @t Plowman.Connection
+  @target Plowman.Connection
+
+  alias Plowman.GitCli.CliState, as: CliState
 
   setup_all do
-    mock = Mock.new(:ssh_connection)
-    {:ok, [mock: mock]}
+    {:ok, [
+      client: CliState.new(cm: :cm, channel: :channelId),
+      conn: Mock.new(:ssh_connection, [stub_all: :ok])
+    ]}
   end
 
-  test "call reply_request to failure", meta do
-    meta[:mock].stubs(:reply_request, [:cm, false, :failure, :channelId], { :ok })
-    assert {:ok} === @t.reply_failure(:cm, false, :channelId)
+  teardown meta do
+    meta[:conn].reset!
+  end
 
-    meta[:mock].stubs(:reply_request, [:cm1, true, :failure, :channelId], { :ok })
-    assert {:ok} === @t.reply_failure(:cm1, true, :channelId)
+  test "call reply_request to failure" do
+    assert :ok == @target.reply_failure(:cm, false, :channelId)
+    assert :ok == @target.reply_failure(:cm1, true, :channelId)
   end
 
   test "call send to failure with mensagem", meta do
-    meta[:mock].stubs(:reply_request, [:cm, false, :failure, :channelId], { :ok })
-    meta[:mock].stubs(:send, [:cm, :channelId, 1, :_], :ok)
+    assert :ok == @target.reply_failure(:cm, false, :channelId, 'message')
+    assert :ok == @target.reply_failure(:cm, false, :channelId, "message")
+    assert :ok == @target.reply_failure(:cm, false, :channelId, "")
 
-    assert {:ok} === @t.reply_failure(:cm, false, :channelId, 'message')
-    assert {:ok} === @t.reply_failure(:cm, false, :channelId, "message")
+    assert 2 == meta[:conn].nc(:send, [:_, :_, 1, 'message'])
+  end
 
-    assert 2 == meta[:mock].nc(:send, [:_, :_, 1, 'message'])
+  test "write chars for valid binary to call forward", meta do
+    client = meta[:client]
+
+    assert :ok == @target.forward(client, "foo")
+    assert 1 == meta[:conn].nc(:send, [client.cm, client.channel, 0, 'foo'])
+  end
+
+  test "write in error channel invalid date", meta do
+    client = meta[:client]
+
+    assert :ok == @target.forward(client, "E:foo")
+    assert 1 == meta[:conn].nc(:send, [client.cm, client.channel, 1, 'foo'])
+  end
+
+  test "send exit code 0 and close connection for eof", meta do
+    client = meta[:client]
+
+    assert :ok == @target.forward(client, :eof)
+    assert 1 == meta[:conn].nc(:exit_status, [client.cm, client.channel, 0])
+    assert 1 == meta[:conn].nc(:close, [client.cm, client.channel])
+  end
+
+  test "send a fail message, exit code 1 and close connection for error", meta do
+    client = meta[:client]
+
+    assert :ok == @target.forward(client, {:error, :reason})
+    msg = '! Unable to contact build server.'
+    assert 1 == meta[:conn].nc(:reply_request, [client.cm, false, :failure, client.channel])
+    assert 1 == meta[:conn].nc(:send, [client.cm, client.channel, 1, msg])
+    assert 1 == meta[:conn].nc(:exit_status, [client.cm, client.channel, 1])
+    assert 1 == meta[:conn].nc(:close, [client.cm, client.channel])
   end
 
   teardown_all meta do
-    meta[:mock].destroy
+    meta[:conn].destroy
   end
 end
